9b2182d3ad57cbce18caecaa3c706eb5c8746a12
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-14 13:26:16 -0400

    hopefully hosting on a port

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -11,29 +11,5 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  runSearch();
-  console.log("Connected!");
+  console.log("Connected! Hosting on Port: " + port);
 }); 
-
-function runSearch() {
-
-}
-
-function artistSearch() {
-    inquirer.prompt({
-        name: "artist",
-        type: "input",
-        message: "What artist would you like to search for?"
-    }).then(function(answer) {
-        var query = "SELECT position, song, year FROM top5000 WHERE ?";
-        con.query(query, {
-            artist: answer.artist
-        }, function (err, res) {
-            if (err) throw err;
-            for (var i=0; i < res.length; i++) {
-                console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-            }
-            runSearch();
-        });
-    });
-}
